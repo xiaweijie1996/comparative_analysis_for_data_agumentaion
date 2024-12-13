a8e142b8651829fa6cfea90d2d44323fb0af7f0f
xiaweijie1996 <xiaweijie1996@gmail.com>
2024-12-13 17:10:09 +0100

    dataloader update

--- data_process/data_loader.py
+++ data_process/data_loader.py
@@ -10,14 +10,91 @@ with open("data_process/config.yaml", "r") as file:
 def catch_the_wind(path=config["Data"]["path"]):
     # Read the cleaned dataset
     data = pd.read_csv(path, index_col=0)
-    print(wind_data.head())
     
     # Abstrat the wind related data
-    wind_data = data[["ref_datetime", "valid_datetime", "WindSpeed", "Wind_MWh_credit"]]
-    print(wind_data.head())
+    wind_data = data[["valid_datetime", "WindSpeed", "Wind_MWh_credit"]]
+
+    # Transform the datetime
+    wind_data["valid_datetime"] = pd.to_datetime(wind_data["valid_datetime"])
+    wind_data["valid_datetime"] = wind_data["valid_datetime"].dt.dayofyear
     
-    pass
+    # Aggregate the data by ref_datetime
+    wind_data = wind_data.groupby("ref_datetime").agg({"valid_datetime":'mean', "WindSpeed":list, "Wind_MWh_credit":list}).reset_index()
+    
+    # Rename the columns
+    windspeed_dataframe = wind_data['WindSpeed'].apply(pd.Series).add_prefix('WindSpeed_')
+    day_dataframe = wind_data['valid_datetime']
+    wind_credit_dataframe = wind_data['Wind_MWh_credit'].apply(pd.Series).add_prefix('Wind_MWh_credit_')
+    
+    # Concatenate the dataframes
+    full_input_data = pd.concat([day_dataframe, windspeed_dataframe], axis=1)
+    full_output_data = wind_credit_dataframe
+    
+    return full_input_data, full_output_data
+
+def split_train_test_val(full_input_data, full_output_data,
+                         split_ratios = config["Data"]["train_val_test_split"]):
+    # Split the data into train, validation and test sets based on the split ratios (default 70-15-15)
+    train_ratio, val_ratio, test_ratio = split_ratios
+    train_size = int(len(full_input_data) * train_ratio)
+    val_size = int(len(full_input_data) * val_ratio)
+    test_size = len(full_input_data) - train_size - val_size
+    
+    # Split the full input data
+    train_input = full_input_data[:train_size]
+    val_input = full_input_data[train_size:train_size + val_size]
+    test_input = full_input_data[-test_size:]
+    
+    # Split the full output data
+    train_output = full_output_data[:train_size]
+    val_output = full_output_data[train_size:train_size + val_size]
+    test_output = full_output_data[-test_size:]
+    
+    # Define dictionary to store the data
+    data_dict = {
+        "train_input": train_input,
+        "val_input": val_input,
+        "test_input": test_input,
+        "train_output": train_output,
+        "val_output": val_output,
+        "test_output": test_output
+    }
+    print('Train input shape:', train_input.shape)
+    print('Train output shape:', train_output.shape)
+    print('Validation input shape:', val_input.shape)
+    print('Validation output shape:', val_output.shape)
+    print('Test input shape:', test_input.shape)
+    print('Test output shape:', test_output.shape)
+    
+    return data_dict
+
+def train_data_reserve(data_dict, reserve_ratio):
+    # Only use reserve_ratio of the training data for training
+    # Split the data into train and reserve sets based on the reserve ratio
+    reserve_size = int(len(data_dict["train_input"]) * reserve_ratio)
+    
+    # Split the train data
+    reserve_input = data_dict["train_input"][:reserve_size]
+    reserve_output = data_dict["train_output"][:reserve_size]
+    
+    # Update the train data
+    data_dict["train_input"] = data_dict["train_input"][reserve_size:]
+    data_dict["train_output"] = data_dict["train_output"][reserve_size:]
+    
+    # Define dictionary to store the data
+    data_dict["reserve_input"] = reserve_input
+    data_dict["reserve_output"] = reserve_output
+    
+    print('Train input shape:', data_dict["train_input"].shape)
+    print('Train output shape:', data_dict["train_output"].shape)
+    return data_dict
+    
 
 if __name__ == "__main__":
-    catch_the_wind()
+    # Catch the wind data
+    full_input_data, full_output_data = catch_the_wind()
+    
+    # Split the data into train, validation and test sets
+    data_dict = split_train_test_val(full_input_data, full_output_data)
+    
     
