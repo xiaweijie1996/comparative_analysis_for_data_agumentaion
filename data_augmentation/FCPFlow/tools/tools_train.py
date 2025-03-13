fad5b61e64a4743ac7fde615d9373ed54048bee3
Weijie Xia <84010474+xiaweijie1996@users.noreply.github.com>
2025-03-13 17:06:38 +0100

    Update tools_train.py
    adopt to new format

--- data_augmentation/FCPFlow/tools/tools_train.py
+++ data_augmentation/FCPFlow/tools/tools_train.py
@@ -1,4 +1,6 @@
 import os
+# import sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import StandardScaler, MinMaxScaler  
@@ -8,10 +10,44 @@ import numpy as np
 import wandb
 import time
 import psutil
-
-import tools.evaluation_m as em
+import pandas as pd
+# import tools.evaluation_m as em
 
 torch.set_default_dtype(torch.float64)
+
+class Datareshape():
+    def __init__(self, data_path):
+        self.dataframe = pd.read_csv(data_path, index_col=0)
+        self.length = self.dataframe.shape[0]
+        self.width = self.dataframe.shape[1]
+        self.add_month_hour()
+        
+    def add_month_hour(self):
+        # Add month and hour to the dataframe
+        # Change the index to datetime
+        self.dataframe.index = pd.to_datetime(self.dataframe.index)
+        self.dataframe['month'] = self.dataframe.index.month
+        self.dataframe['hour'] = self.dataframe.index.hour
+        
+        # Put month and hour before the last column
+        cols = list(self.dataframe.columns)
+        cols = cols[:-3] + cols[-2:] + cols[-3:-2]
+        self.dataframe = self.dataframe[cols]
+        
+        
+    
+    def creat_new_frame(self):
+        _num_step = 48
+        new_frame = pd.DataFrame()
+        for i in range(self.length-_num_step):
+            _data = self.dataframe.iloc[i:i+_num_step, :]
+            _data = _data.values
+            _x = _data[:, :-1].reshape(1, -1)
+            _y = _data[:, -1:].reshape(1, -1)
+            _data = np.hstack((_x, _y))
+            new_frame = pd.concat([new_frame, pd.DataFrame(_data)], axis=0)
+        return new_frame
+        
 
 def create_data_loader(numpy_array, batch_size=32, scaler = StandardScaler(), shuffle=True):
     # Check if nan exists in the data, if nan drop
@@ -200,4 +236,18 @@ def train(model, train_loader, optimizer, epochs, cond_dim,
         # ----------------- Test the model -----------------
         print(epoch, 'loss LogLikelihood: ', loss.item(),'Memory Usage:', psutil.virtual_memory().percent) # , 'loss Distance: ', loss_test.item())
 
-        
+    
+if __name__ == "__main__":
+    
+    # Test the Datareshape class
+    data_path = 'dsets/percentage/continuous/5percent_dataset.csv'
+    data_reshape = Datareshape(data_path)
+    print(data_reshape.dataframe.head())
+    new_frame = data_reshape.creat_new_frame()
+    print(new_frame.head())
+    
+    # Test the create_data_loader function
+    data = new_frame.values
+    loader, scaler = create_data_loader(data, batch_size=32)
+    _data = next(iter(loader))[0]
+    print(_data.shape)
