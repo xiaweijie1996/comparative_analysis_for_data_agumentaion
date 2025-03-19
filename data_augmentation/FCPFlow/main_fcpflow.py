5fde3df5cc331b0b7893a790289977eb5261e6b1
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-03-19 19:59:58 +0100

    gix memory bug

--- data_augmentation/FCPFlow/main_fcpflow.py
+++ data_augmentation/FCPFlow/main_fcpflow.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     FCPflow.to(device)
     print('Number of parameters: ', sum(p.numel() for p in FCPflow.parameters()))
         
-    for _index in [ 0.1, 0.3, 0.5, 0.8, 1.0]: # 0.05,
+    for _index in [ 0.05, 0.1, 0.3, 0.5, 0.8, 1.0]: # 0.05,
         
         wandb.init() # Initialize the wandb
 
