4511a4b8ba67251efead2bf11322746734784834
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-03-25 19:04:59 +0100

    bath up

--- data_augmentation/FCPFlow/main_fcpflow-2.py
+++ data_augmentation/FCPFlow/main_fcpflow-2.py
@@ -39,11 +39,11 @@ if __name__ == '__main__':
     scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, step_size_up=config["FCPflow"]["lr_step_size"], 
                                             base_lr=config["FCPflow"]["lr_min"], max_lr=config["FCPflow"]["lr_max"],
                                                        cycle_momentum=False)
-    # # Load saved model
-    # _path = 'data_augmentation/FCPFlow/saved_model/FCPflow_model_0.5.pth'
+    # Load saved model
+    # _path = 'data_augmentation/FCPFlow/saved_model/FCPflow_model_0.05.pth'
     # FCPflow.load_state_dict(torch.load(_path))
     
-    for _index in [0.05, 0.1, 0.3, 0.5, 0.8, 1.0]: # 0.05, 0.05, 0.1,  0.5, 
+    for _index in [0.1, 0.3, 0.5, 0.8, 1.0]: # 0.05, 0.05, 0.1,  0.5, 
         
         wandb.init(project="fcpflow", name=f"FCPflow_{_index*100}percent", reinit=True)
         # log the number of parameters
